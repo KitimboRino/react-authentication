e56a744a43d79c74968bbd759e0cedf619801628
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-03-17 02:45:31 +0300

    created a useUser hook

--- front-end/src/auth/useUser.js
+++ front-end/src/auth/useUser.js
@@ -2,5 +2,29 @@ import { useState, useEffect } from "react";
 import { setToken } from './useToken';
 
 export const useUser = () => {
+    const [token] = useToken();
 
+    // Function for getting payload from the token
+    const getPayloadFromToken = (token) => {
+        const encodedPayload = token.split('.')[1];
+        return JSON.parse(atob(encodedPayload));
+    }
+
+    // Defining a user state
+    const [user, setUser] = useState(() =>{
+        if(!token) return null;
+        return getPayloadFromToken(token);
+    })
+
+    // Watching the token for changes
+    useEffect(() => {
+        if(token) {
+            const { id, isVerified, email, info } = getPayloadFromToken(token);
+            setUser({ id, isVerified, email, info });
+        } else {
+            setUser(null);
+        }
+    }, [token]);
+
+    return user;
 }
